5597d1c51c0631dc4acb8c7552c1a34a9064267c
Aaron <will22thomas@gmail.com>
2023-03-20 09:16:36 -0600

    made some changes

--- cpp/lidar/send_lidar.cpp
+++ cpp/lidar/send_lidar.cpp
@@ -7,13 +7,11 @@ using namespace std;
 
 // Creates an item object and fills it with the provided data.
 // Returns the item object
-lidar_package::item newItem(string timestamp, int64_t framecount, float coordinates, int64_t intensity)
+lidar_package::item newItem(string timestamp, int64_t framecount)
 {
     lidar_package::item item;
     item.timestamp = timestamp;
     item.framecount = framecount;
-    item.coordinates = coordinates;
-    item.intensity = intensity;
     return item;
 }
 
@@ -24,32 +22,22 @@ int main(int argc, char** argv)
 {
     // Create LCM object and check that it was created successfully
     lcm::LCM lcm;
-    if (!lcm.good())
-    {
+    if (!lcm.good()) {
         std::cout << "LCM initialization failed" << std::endl;
         return 1;
     }
     // If no command line arguments are provided, run the program with a single item message,
-    if (argc <= 1)
-    {
+    if (argc <= 1) {
         lidar_package::item msg;
-        msg = newItem("Milk", 5, true);
+        msg = newItem("Milk", 5);
         // Specifies the channel name for the message and publishes it
         lcm.publish("INDIVIDUAL", &msg);
     }
     // However, if any arguments are provided then run it with a hardcoded status message.
-    else
-    {
-        std::string names[5] = {"Milk", "Cheese", "Ground Beef", "Salt", "Pepper"};
-        int16_t costs[5] = {5, 30, 6, 1, 2};
-        bool is_stocked[5] = {true, true, false, true, false};
-        lidar_package::status msg;
+    else {
+        lidar_package::item msg;
         msg.timestamp = std::chrono::system_clock::now().time_since_epoch().count();
-        msg.item_count = 5;
-        for (int i = 0; i < 5; i++)
-        {
-            msg.items.push_back(newItem(names[i], costs[i], is_stocked[i]));
-        }
+        msg.framecount = 5;
         // Specifies the channel name for the message and publishes it
         lcm.publish("REPORT", &msg);
     }
