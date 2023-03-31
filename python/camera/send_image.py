0ac7a73e9f8b45d16587425b8526acaad6ed7549
Aaron <will22thomas@gmail.com>
2023-03-31 09:33:48 -0600

    Got camera to work sort of

--- python/camera/send_image.py
+++ python/camera/send_image.py
@@ -1,40 +1,50 @@
 import cv2
-# from vimba import *
+from vimba import *
 from datetime import datetime
 import lcm
-import camera_image_t
+from camera import camera_image_t
 from PIL import Image
 
 
-def stream_live_data(hostname, port):
+def stream_live_data():
     msg = camera_image_t()
-    msg.camera_name = hostname
-    msg.jpeg_image = "/home/"
+    # msg.camera_name = hostname
+    # msg.jpeg_image = "/home/"
 
-    msg.timestamp = "date month year"
-    msg.framecount = 0
+    # msg.timestamp = "date month year"
+    # msg.framecount = 0
 
     lc = lcm.LCM()
+
+    vimba = Vimba. get_instance ()
+    vimba.enable_log ( LOG_CONFIG_WARNING_CONSOLE_ONLY )
+    log = Log.get_instance ()
+    log.critical ('Critical , visible ')
+    log.error ('Error , visible ')
+    log.warning ('Warning , visible ')
+    log.info('Info , invisible ')
+    log.trace ('Trace , invisible ')
 
     with Vimba.get_instance() as vimba:
         cams = vimba.get_all_cameras()
         with cams[0] as cam:
             frame = cam.get_frame()
-            frame.convert_pixel_format(PixelFormat.Mono8)
-            cv2.imwrite('frame.jpeg'), frame.as_opencv_image()
+            frame.convert_pixel_format(PixelFormat.Bgr8)
+            cv2.imwrite('frame.jpeg', frame.as_opencv_image())
+
+    vimba.disable_log ()
 
     time_part = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    msg.timestamp = time_part
-    msg.framecount += 1
+    # msg.timestamp = time_part
+    # msg.framecount += 1
     
     # publish data
-    lc.publish("DATA", msg.encode())
+    # lc.publish("DATA", msg.encode())
 
 def test_lcm():
     msg = camera_image_t()
     msg.camera_name = "Name"
     myImage = Image.open("/home/Downloads/random-grid.jpg")
-    myImage.show()
     msg.jpeg_image = myImage
 
     msg.timestamp = "date month year"
@@ -46,8 +56,8 @@ def test_lcm():
 
 
 def main():
-    # stream_live_data("169.254.228.223", 7502)
-    test_lcm()
+    stream_live_data()
+    # test_lcm()
 
 if __name__ == "__main__":
     main()
