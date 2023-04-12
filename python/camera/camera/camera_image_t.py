42725e52904783f07e44f755d2aeef6260981165
Aaron <will22thomas@gmail.com>
2023-04-11 23:51:40 -0600

    updated camera files to function

--- python/camera/camera/camera_image_t.py
+++ python/camera/camera/camera_image_t.py
@@ -10,11 +10,11 @@ except ImportError:
 import struct
 
 class camera_image_t(object):
-    __slots__ = ["timestamp", "camera_name", "framecount", "width", "height", "row_stride", "size", "data"]
+    __slots__ = ["timestamp", "camera_name", "framecount", "width", "height", "size", "data"]
 
-    __typenames__ = ["string", "string", "int64_t", "int32_t", "int32_t", "int32_t", "int32_t", "byte"]
+    __typenames__ = ["string", "string", "int64_t", "int32_t", "int32_t", "int32_t", "byte"]
 
-    __dimensions__ = [None, None, None, None, None, None, None, ["size"]]
+    __dimensions__ = [None, None, None, None, None, None, ["size"]]
 
     def __init__(self):
         self.timestamp = ""
@@ -22,7 +22,6 @@ class camera_image_t(object):
         self.framecount = 0
         self.width = 0
         self.height = 0
-        self.row_stride = 0
         self.size = 0
         self.data = b""
 
@@ -41,7 +40,7 @@ class camera_image_t(object):
         buf.write(struct.pack('>I', len(__camera_name_encoded)+1))
         buf.write(__camera_name_encoded)
         buf.write(b"\0")
-        buf.write(struct.pack(">qiiii", self.framecount, self.width, self.height, self.row_stride, self.size))
+        buf.write(struct.pack(">qiii", self.framecount, self.width, self.height, self.size))
         buf.write(bytearray(self.data[:self.size]))
 
     def decode(data):
@@ -60,14 +59,14 @@ class camera_image_t(object):
         self.timestamp = buf.read(__timestamp_len)[:-1].decode('utf-8', 'replace')
         __camera_name_len = struct.unpack('>I', buf.read(4))[0]
         self.camera_name = buf.read(__camera_name_len)[:-1].decode('utf-8', 'replace')
-        self.framecount, self.width, self.height, self.row_stride, self.size = struct.unpack(">qiiii", buf.read(24))
+        self.framecount, self.width, self.height, self.size = struct.unpack(">qiii", buf.read(20))
         self.data = buf.read(self.size)
         return self
     _decode_one = staticmethod(_decode_one)
 
     def _get_hash_recursive(parents):
         if camera_image_t in parents: return 0
-        tmphash = (0x33309e8c226d8a19) & 0xffffffffffffffff
+        tmphash = (0x69657fa5762b1b6) & 0xffffffffffffffff
         tmphash  = (((tmphash<<1)&0xffffffffffffffff) + (tmphash>>63)) & 0xffffffffffffffff
         return tmphash
     _get_hash_recursive = staticmethod(_get_hash_recursive)
--- python/camera/send_image.py
+++ python/camera/send_image.py
@@ -4,49 +4,55 @@ from datetime import datetime
 import lcm
 from camera import camera_image_t
 from PIL import Image
+import numpy as np
 
 
 def stream_live_data():
     msg = camera_image_t()
     lc = lcm.LCM()
+    show = True
 
     vimba = Vimba.get_instance ()
     vimba.enable_log ( LOG_CONFIG_WARNING_CONSOLE_ONLY )
     log = Log.get_instance ()
-    log.critical ('Critical , visible ')
-    log.error ('Error , visible ')
-    log.warning ('Warning , visible ')
-    log.info('Info , invisible ')
-    log.trace ('Trace , invisible ')
+    # log.critical ('Critical , visible ')
+    # log.error ('Error , visible ')
+    # log.warning ('Warning , visible ')
+    # log.info('Info , invisible ')
+    # log.trace ('Trace , invisible ')
+    while(show):
+        with Vimba.get_instance() as vimba:
+            cams = vimba.get_all_cameras()
+            with cams[0] as cam:
+                frame = cam.get_frame()
+                frame.convert_pixel_format(PixelFormat.Bgr8)
+                name = datetime.now().strftime("%Y/%m/%d %H:%M:%S") + '.jpeg'
+                print("Printing image...")
+                print(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
+                cv2.imwrite('frame.jpeg', frame.as_opencv_image())
+                img = frame.as_opencv_image()
 
-    with Vimba.get_instance() as vimba:
-        cams = vimba.get_all_cameras()
-        with cams[0] as cam:
-            frame = cam.get_frame()
-            frame.convert_pixel_format(PixelFormat.Bgr8)
-            name = datetime.now().strftime("%Y/%m/%d %H:%M:%S") + '.jpeg'
-            cv2.imwrite(name, frame.as_opencv_image())
-            img = frame.as_opencv_image()
+        vimba.disable_log()
 
-    vimba.disable_log()
+        scale_percent = 10 # percent of original size
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+        dim = (width, height)
 
-    scale_percent = 10 # percent of original size
-    width = int(img.shape[1] * scale_percent / 100)
-    height = int(img.shape[0] * scale_percent / 100)
-    dim = (width, height)
+        cvDown = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
-    cvDown = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        msg.timestamp = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        msg.camera_name = "camera name"
+        msg.width = width
+        msg.height = height
+        msg.framecount += 1
 
-    msg.timestamp = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    msg.camera_name = cams[0]
-    msg.width = width
-    msg.height = height
-
-    msg.size = msg.height * msg.width
-    msg.data = (cvDown.data, cvDown.data + cvDown.all() * len(cvDown.shape))
-    
-    # publish data
-    # lc.publish("DATA", msg.encode())
+        msg.size = msg.height * msg.width
+        msg.data = cvDown
+        # (cvDown.data, cvDown.data + cvDown.all() * len(cvDown.shape))
+        
+        # publish data
+        lc.publish("DATA", msg.encode())
 
 def test_lcm():
     msg = camera_image_t()
